1f42cd1d35e8a32fbb218c673bca0ff71570306e
freedomfeng <18844545194@163.com>
2017-02-22 22:50:29 +0800

    add the src codes and some configuration files

---
+++ src/com/jlu/test/GitTest.java
@@ -0,0 +1,14 @@
+package com.jlu.test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User:Administrator
+ * Date:2017/2/22
+ * Time:22:48
+ * see:
+ */
+public class GitTest {
+    public static void main(String[] args){
+        System.out.println("this is a git test");
+    }
+}
